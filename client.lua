3587b938d176cf8c84a1369fcd0ac998b69d0abb
Pringles <theyork342@hotmail.com>
2019-08-16 15:25:03 -0300

    Update client.lua

--- client.lua
+++ client.lua
@@ -5,9 +5,9 @@
 Citizen.CreateThread(function()
     while true do
 	N_0x4757f00bc6323cfe(GetHashKey("WEAPON_UNARMED"), 0.1) 
-    Wait(0)
-    N_0x4757f00bc6323cfe(GetHashKey("WEAPON_NIGHTSTICK"), 0.1) 
-    Wait(0)
+    	Wait(0)
+    	N_0x4757f00bc6323cfe(GetHashKey("WEAPON_NIGHTSTICK"), 0.1) 
+    	Wait(0)
     end
 end)
 --------------------------------------
@@ -16,11 +16,11 @@ end)
 Citizen.CreateThread(function()
     while true do
         Citizen.Wait(0)
-		local ped = PlayerPedId()
+	local ped = PlayerPedId()
         if IsPedArmed(ped, 6) then
-	        DisableControlAction(1, 140, true)
-        	DisableControlAction(1, 141, true)
-           	DisableControlAction(1, 142, true)
+	   DisableControlAction(1, 140, true)
+       	   DisableControlAction(1, 141, true)
+           DisableControlAction(1, 142, true)
         end
     end
 end)
