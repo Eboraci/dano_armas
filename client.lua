5ac8763429b9486c7f6760c7b7383e2d12e18be7
Pringles <theyork342@hotmail.com>
2019-08-16 15:23:09 -0300

    Add files via upload

---
+++ client.lua
@@ -0,0 +1,26 @@
+
+--------------------------------------
+--DANO ARMAS (E SOCO) MELEE
+--------------------------------------
+Citizen.CreateThread(function()
+    while true do
+	N_0x4757f00bc6323cfe(GetHashKey("WEAPON_UNARMED"), 0.1) 
+    Wait(0)
+    N_0x4757f00bc6323cfe(GetHashKey("WEAPON_NIGHTSTICK"), 0.1) 
+    Wait(0)
+    end
+end)
+--------------------------------------
+--DANO CORONHADA
+--------------------------------------
+Citizen.CreateThread(function()
+    while true do
+        Citizen.Wait(0)
+		local ped = PlayerPedId()
+        if IsPedArmed(ped, 6) then
+	        DisableControlAction(1, 140, true)
+        	DisableControlAction(1, 141, true)
+           	DisableControlAction(1, 142, true)
+        end
+    end
+end)
